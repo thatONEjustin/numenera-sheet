067ad81b82a3d8a913b01165e053ca58fe313d3e
Justin P <2763180+thatONEjustin@users.noreply.github.com>
2025-01-27 21:43:23 -0600

    main

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,15 @@ import { defineConfig } from 'astro/config';
 
 import svelte from '@astrojs/svelte';
 
+import node from '@astrojs/node';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [svelte()]
+    integrations: [svelte()],
+
+    output: 'server',
+
+    adapter: node({
+        mode: 'standalone'
+    })
 });
