4818ffffb28daa91132f9de885cf8190c0c4bda8
thatONEjustin <2763180+thatONEjustin@users.noreply.github.com>
2025-02-01 09:34:09 -0600

    trying vite again
    - tbh i'm gonna go back to postcss because LSP support seemed a lot
    better

---
+++ .netlify/build/entry.mjs
@@ -0,0 +1,29 @@
+import { renderers } from './renderers.mjs';
+import { s as serverEntrypointModule } from './chunks/_@astrojs-ssr-adapter_CvSoi7hX.mjs';
+import { manifest } from './manifest_BoUbhb0t.mjs';
+import { createExports } from '@astrojs/netlify/ssr-function.js';
+
+const serverIslandMap = new Map();;
+
+const _page0 = () => import('./pages/index.astro.mjs');
+const pageMap = new Map([
+    ["src/pages/index.astro", _page0]
+]);
+
+const _manifest = Object.assign(manifest, {
+    pageMap,
+    serverIslandMap,
+    renderers,
+    middleware: () => import('./_noop-middleware.mjs')
+});
+const _args = {
+    "middlewareSecret": "cd604503-ab2e-443f-bde0-79d269fadc6b"
+};
+const _exports = createExports(_manifest, _args);
+const __astrojsSsrVirtualEntry = _exports.default;
+const _start = 'start';
+if (_start in serverEntrypointModule) {
+	serverEntrypointModule[_start](_manifest, _args);
+}
+
+export { __astrojsSsrVirtualEntry as default, pageMap };
